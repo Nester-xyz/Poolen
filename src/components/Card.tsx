f8be3eabd472ea9a2ba39ba59c5d2324d108e61f
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-23 14:35:25 +0545

    style(card): update style of card to show coin and move description up

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+import { ConvertToCoin, CutString } from "../lib/helper";
 import { TBetCard } from "../types/list";
 
 type CardProps = {
@@ -7,24 +8,30 @@ const Card = ({ card }: CardProps) => {
   return (
     <div className="w-full h-40 border border-black rounded-tl-3xl rounded-br-3xl flex justify-between items-center px-5">
       {/* <div className="border triangle absolute bg-red-200"> */}
-      <div>{card.description}</div>
-      <div className="grid grid-cols-2">
-        {card.options.map((data, index) => {
-          return (
-            <div key={index} className="flex items-center gap-2">
-              <div className="flex items-center gap-2">
-                <img src={data.icon} alt="icon" className="w-8 h-8" />
-                <div className="text-lg">{data.name}</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-      <div className="">
-        <div className="text-md">Prize pool</div>
-        <div className="text-5xl">{card.prizePool}</div>
-        <div className="text-3xl">$NST</div>
-        {/* </div> */}
+      <div className="grid grid-cols-5">
+        <div className="col-span-4">
+          {CutString(card.description, 100)}
+          <div className="grid grid-cols-4">
+            {card.options.map((data, index) => {
+              return (
+                <div key={index} className="flex flex-col items-center gap-1">
+                  <img
+                    src={data.icon}
+                    alt="icon"
+                    className="w-8 h-8 rounded-full object-cover"
+                  />
+                  <div className="text-lg">{ConvertToCoin(data.name)}</div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="text-md">Prize pool</div>
+          <div className="text-5xl">{card.prizePool}</div>
+          <div className="text-3xl">$NST</div>
+          {/* </div> */}
+        </div>
       </div>
     </div>
   );
